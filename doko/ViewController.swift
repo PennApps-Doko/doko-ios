8164a5919b3a33b26c58dacb095091cce8f1a399
ey1031 <eyudeveloper@gmail.com>
2018-09-08 01:49:41 -0400

    initialize project and storyboard elements

---
+++ doko/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  doko
+//
+//  Created by Emily on 9/8/18.
+//  Copyright © 2018 Emily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func login(_ sender: Any) {
+    }
+    @IBAction func login_2(_ sender: Any) {
+    }
+    @IBAction func login_3(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
